04eb513e3d7c9864ee4f8229e2dea5ca54cf2703
Efrem-Yohanis <efremyohanis116@gmail.com>
2026-03-21 11:17:56 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
+import { CampaignProvider } from "@/context/CampaignContext";
+import AppShell from "@/components/AppShell";
+import CampaignList from "@/pages/CampaignList";
+import CampaignDetail from "@/pages/CampaignDetail";
+import CampaignCreate from "@/pages/CampaignCreate";
+import AudienceList from "@/pages/AudienceList";
+import AudienceDetail from "@/pages/AudienceDetail";
+import ScheduleList from "@/pages/ScheduleList";
+import ScheduleDetail from "@/pages/ScheduleDetail";
+import MessageContentList from "@/pages/MessageContentList";
+import MessageContentDetail from "@/pages/MessageContentDetail";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -14,11 +24,23 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <CampaignProvider>
+          <AppShell>
+            <Routes>
+              <Route path="/" element={<CampaignList />} />
+              <Route path="/campaigns/new" element={<CampaignCreate />} />
+              <Route path="/campaigns/:id" element={<CampaignDetail />} />
+              <Route path="/campaigns/:id/edit" element={<CampaignCreate />} />
+              <Route path="/audiences" element={<AudienceList />} />
+              <Route path="/audiences/:id" element={<AudienceDetail />} />
+              <Route path="/schedules" element={<ScheduleList />} />
+              <Route path="/schedules/:id" element={<ScheduleDetail />} />
+              <Route path="/messages" element={<MessageContentList />} />
+              <Route path="/messages/:id" element={<MessageContentDetail />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </AppShell>
+        </CampaignProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
